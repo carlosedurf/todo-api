80989ae4007aa56fa757321b55f06b1d619b1ecb
Carlos Eduardo <caroseduardo.rocha.f@gmail.com>
2024-08-03 23:35:06 -0300

    Add ability with role

--- tests/Feature/Models/Permissions/RoleTest.php
+++ tests/Feature/Models/Permissions/RoleTest.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Models\Ability;
 use App\Models\Role;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 
 uses(RefreshDatabase::class);
+
+dataset('role', [
+    fn () => Role::factory()->createOne(),
+]);
+
+dataset('ability', [
+    fn () => Ability::factory()->createOne(),
+]);
 
 it('can create a role', function (Role $role) {
     expect(Role::find($role->id)->exists())->toBeTrue();
@@ -11,3 +21,20 @@ it('can create a role', function (Role $role) {
     ->with([
         fn () => Role::factory()->createOne(),
     ]);
+
+it('Can linked ability with a role', function (Role $role, Ability $ability) {
+    $role->abilities()->attach($ability);
+    $exists = DB::table('ability_role')
+        ->where('role_id', $role->id)
+        ->where('ability_id', $ability->id)
+        ->exists();
+    expect($exists)->toBe(true);
+})
+    ->with('role')
+    ->with('ability');
+
+it('Can listing abilities by role', function (Role $role) {
+    Ability::factory()->count(10)->create()->each(fn ($ability) => $role->abilities()->attach($ability));
+    $role->refresh();
+    expect($role->abilities)->toHaveCount(10);
+})->with('role');
